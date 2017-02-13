765e418551ea1d43b656d7fbbfa9e3c59c3ea604
Grace <gracefctorres@gmail.com>
2017-02-13 12:54:47 -0500

    changes-names-documents-add-js

---
+++ js/signup.js
@@ -0,0 +1,16 @@
+function init(){
+    var button=document.getElementById("button");
+    button.addEventListener("click",);
+    if(localStorage.getItem("country")){
+        setCountry();
+    }
+}
+function setCountry(){
+    var cname=document.getElementById("country-name");
+    cname=
+}
+function onbuttonclick(){
+    var input=document.getElementById("numbers");
+    
+    
+}
