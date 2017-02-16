4a5b3447f11d683ac17b89741b022f34e55cbcee
Grace <gracefctorres@gmail.com>
2017-02-16 16:53:16 -0500

    adding javascript flags

--- js/countries-name.js
+++ js/countries-name.js
@@ -1,15 +1,15 @@
 function init(){
 var flags = document.getElementById("paisDates");
-var jsoncountry='[{"nombre": "Australia","code": "61", "image":"images/flags/flat/16/Australia.png"},'+
-'{"nombre":"Canada","code":"1","image":"images/flags/flat/16/Canada.png"},'+
-'{"nombre":"China","code":"86","image":"images/flags/flat/16/China.png"},'+
-'{"nombre":"France","code": "33", "image":"images/flags/flat/16/France.png"},'+
-'{"nombre": "India","code": "91","image":"images/flags/flat/16/India.png"},'+
-'{"nombre": "Japan","code": "81", "image":"images/flags/flat/16/Japan.png"},'+
-'{"nombre": "Mexico","code": "52", "image":"images/flags/flat/16/Mexico.png"},'+
-'{"nombre": "Peru","code": "51", "image":"images/flags/flat/16/Peru.png"},'+
-'{"nombre": "Puerto Rico","code": "1", "image":"images/flags/flat/16/Puerto-Rico.png"},'+
-'{"nombre": "United Kingdom","code": "44","image":"images/flags/flat/16/United-Kingdom.png"}]';
+var jsoncountry='[{"nombre": "Australia","code":"+61", "image":"images/flags/flat/16/Australia.png"},'+
+'{"nombre":"Canada","code":"+1","image":"images/flags/flat/16/Canada.png"},'+
+'{"nombre":"China","code":"+86","image":"images/flags/flat/16/China.png"},'+
+'{"nombre":"France","code": "+33", "image":"images/flags/flat/16/France.png"},'+
+'{"nombre": "India","code": "+91","image":"images/flags/flat/16/India.png"},'+
+'{"nombre": "Japan","code": "+81", "image":"images/flags/flat/16/Japan.png"},'+
+'{"nombre": "Mexico","code": "+52", "image":"images/flags/flat/16/Mexico.png"},'+
+'{"nombre": "Peru","code": "+51", "image":"images/flags/flat/16/Peru.png"},'+
+'{"nombre": "Puerto Rico","code": "+1", "image":"images/flags/flat/16/Puerto-Rico.png"},'+
+'{"nombre": "United Kingdom","code": "+44","image":"images/flags/flat/16/United-Kingdom.png"}]';
 
 
 var paises = JSON.parse(jsoncountry);
@@ -34,6 +34,7 @@ var long = paises.length;
 function onClick(evt)
 {
     var srcImage=evt.currentTarget.getElementsByClassName("bandera")[0].src;
+    console.log(srcImage);
     var codeCountry=evt.currentTarget.getElementsByClassName("codephone")[0].textContent;
 
     localStorage.setItem("cImage",srcImage);
