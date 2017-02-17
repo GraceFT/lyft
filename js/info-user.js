894fb06631f9b7967742a60f4a47ad50a267fe40
Grace <gracefctorres@gmail.com>
2017-02-17 11:30:58 -0500

    validating form

--- js/info-user.js
+++ js/info-user.js
@@ -1,6 +1,6 @@
 function validateForm(){
     if (validateName()==true && validateLastName()==true){
-        avise.innerHTML='<span style="color:green; font-size:11px; font-style:italic;">Validate name and lastname</span>';
+        avise.innerHTML='<span style="color:#ff00bf; font-size:11px; font-style:italic;">Validate name and lastname</span>';
         var next= document.getElementById("btn_next");
         next.href="app.html";
     }else{
@@ -16,39 +16,31 @@ function upperCaseName(_id){
 //validating name
 function validateName(){
     var valName = document.getElementById("name").value;
-    var isName = true;
+    var validaName= true;
     if (valName!= ""){
-        upperCaseName("name");
-        return isName = true;
-        if(/([0-9]+)/.test(valName)){
-        return isName = false;
-        }else{
-        return isName = true;    
+        upperCaseName("name"); 
+        if(!/([0-9]+)/.test(valName)){
+        return true;
+        localStorage.setItem("name",valName);
         }
     }
 }
 //validating lastname
 function validateLastName(){
     var valLastName = document.getElementById("lastname").value;
-    var isLastName=true;
     if (valLastName!= ""){
-        upperCaseName("lastname");
-        return isLastName = true;
-        if(/([0-9]+)/.test(valLastName)){
-        return isLastName = false;
-        }else{
-        return isLastName = true;    
+        upperCaseName("lastname"); 
+        if(!/([0-9]+)/.test(valLastName)){
+        localStorage.setItem("lastname",valLastName);
+        return true;}    
     }
-}
 }
 //validating email
 function validateEmail(){
-    var valEmail = document.getElementById("input-email").value;
-    var isEmail=true;
-    if(!/^([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})$/.test(valEmail)){
-    return isEmail=false;
-    }else{
-        emailadv.innerHTML="<span style='color:blue; font-weight:bolder'>Campo Valido</span>";
-        return isEmail=true;
-    }  
+    var valEmail = document.getElementById("email").value;
+    var text=/^([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})$/;
+    if(text.test(valEmail)){
+    localStorage.setItem("email",val)
+        return true;
+    }
 }
