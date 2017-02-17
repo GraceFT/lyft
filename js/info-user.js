0d4b7e99d628d6e636a0017646fbb6f78c1ed923
Grace <gracefctorres@gmail.com>
2017-02-17 10:32:59 -0500

    js-info-user js-signup

--- js/info-user.js
+++ js/info-user.js
@@ -1,8 +1,10 @@
 function validateForm(){
-var span= document.getElementById("button-next");
-if (validateName()==true && validateLastName()==true && validateEmail()==true){
-    var next = document.createElement("a");
-    span.appendChild(next);
+    if (validateName()==true && validateLastName()==true){
+        avise.innerHTML='<span style="color:green; font-size:11px; font-style:italic;">Validate name and lastname</span>';
+        var next= document.getElementById("btn_next");
+        next.href="app.html";
+    }else{
+        avise.innerHTML='<span style="color:#ff00bf; font-size:11px; font-style:italic;">Complete with your name and lastname</span>';
     }
 }
 //Uppercase
@@ -14,27 +16,39 @@ function upperCaseName(_id){
 //validating name
 function validateName(){
     var valName = document.getElementById("name").value;
-    var validaName= true;
+    var isName = true;
     if (valName!= ""){
-        upperCaseName("name"); 
-        if(!/([0-9]+)/.test(valName)){
-        return true;}
+        upperCaseName("name");
+        return isName = true;
+        if(/([0-9]+)/.test(valName)){
+        return isName = false;
+        }else{
+        return isName = true;    
+        }
     }
 }
 //validating lastname
 function validateLastName(){
     var valLastName = document.getElementById("lastname").value;
+    var isLastName=true;
     if (valLastName!= ""){
-        upperCaseName("lastname"); 
-        if(!/([0-9]+)/.test(valLastName)){
-        return true;}    
+        upperCaseName("lastname");
+        return isLastName = true;
+        if(/([0-9]+)/.test(valLastName)){
+        return isLastName = false;
+        }else{
+        return isLastName = true;    
     }
+}
 }
 //validating email
 function validateEmail(){
-    var valEmail = document.getElementById("email").value;
-    var text=/^([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})$/;
-    if(text.test(valEmail)){
-        return true;
-    }
+    var valEmail = document.getElementById("input-email").value;
+    var isEmail=true;
+    if(!/^([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})$/.test(valEmail)){
+    return isEmail=false;
+    }else{
+        emailadv.innerHTML="<span style='color:blue; font-weight:bolder'>Campo Valido</span>";
+        return isEmail=true;
+    }  
 }
--- js/signup.js
+++ js/signup.js
@@ -20,21 +20,25 @@ function toDoAnAlert(){
         var completeCode= code + Math.floor(number);
         localStorage.setItem('avatarNumb',completeCode);
         alert("Your code: " + completeCode);
-        console.log(btn-next.href)
-    }else{
-    } 
+        var next= document.getElementById("btn-next");
+        next.href="info-user.html";
+    }
 }
 function numberLong(){
     var userNumber = document.getElementById("user-number");
+    var isVal= true;
     if(userNumber.value.length==0){
-        avise.innerHTML='<span style="color:#ff00bf; font-size:11px; font-style:italic;">Complete with your phone number</span>'
+        avise.innerHTML='<span style="color:#ff00bf; font-size:11px; font-style:italic;">Complete with your phone number</span>';
+        return isVal=false;
     }else{
-        if(userNumber.value.length > 9){
-        avise.innerHTML='<span style="color:#ff00bf; font-size:11px; font-style:italic;">Put a number less than 9 digits</span>'
+        if(userNumber.value.length > 9 || userNumber.value.length < 6){
+        avise.innerHTML='<span style="color:#ff00bf; font-size:11px; font-style:italic;">Put a number more than 6 digits and less than 9 digits</span>';
+        return isVal= false;
         }else{
         avise.innerHTML='<span style="color:green; font-size:11px; font-style:italic;">Correct Number</span>'
-        return true;
+        return isVal=true;
         }
     }
 }
+
 
